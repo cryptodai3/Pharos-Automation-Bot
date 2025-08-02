eb0a236988a266a4ce5f0252afaf037570a9e82c
Cryptodai3 <cryptodai3@gmail.com>
2025-08-02 08:53:51 +0545

    Update bot4.py
    Fix Approving Error

--- bot4.py
+++ bot4.py
@@ -23,9 +23,10 @@ class Brokex:
             "User-Agent": FakeUserAgent().random
         }
         self.BASE_API = "https://proofcrypto-production.up.railway.app"
-        self.RPC_URL = "https://api.zan.top/node/v1/pharos/testnet/54b49326c9f44b6e8730dc5dd4348421"
+        self.RPC_URL = "https://testnet.dplabs-internal.com/"
+        self.PHRS_CONTRACT_ADDRESS = "0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE"
         self.USDT_CONTRACT_ADDRESS = "0x78ac5e2d8a78a8b8e6d10c7b7274b03c10c91cef"
-        self.CLAIM_ROUTER_ADDRESS = "0x50576285BD33261DEe1aD99BF766CD8249520a58"
+        self.FAUCET_ROUTER_ADDRESS = "0x50576285BD33261DEe1aD99BF766CD8249520a58"
         self.TRADE_ROUTER_ADDRESS = "0xDe897635870b3Dd2e097C09f1cd08841DBc3976a"
         self.POOL_ROUTER_ADDRESS = "0x9A88d07850723267DB386C681646217Af7e220d7"
         self.ERC20_CONTRACT_ABI = json.loads('''[
@@ -138,6 +139,12 @@ class Brokex:
             { "name": "ETH_USDT", "desimal": 1 },
             { "name": "SOL_USDT", "desimal": 10 },
             { "name": "XRP_USDT", "desimal": 14 },
+            { "name": "AVAX_USDT", "desimal": 5 },
+            # { "name": "DOGE_USDT", "desimal": 3 },
+            { "name": "TRX_USDT", "desimal": 15 },
+            { "name": "ADA_USDT", "desimal": 16 },
+            { "name": "SUI_USDT", "desimal": 90 },
+            { "name": "LINK_USDT", "desimal": 2 },
         ]
         self.proxies = []
         self.proxy_index = 0
@@ -173,7 +180,7 @@ class Brokex:
         print(Fore.YELLOW + Style.BRIGHT + "    🧑‍💻 Author     : YetiDAO")
         print(Fore.YELLOW + Style.BRIGHT + "    🌐 Status     : Running & Monitoring...")
         print(Fore.CYAN + Style.BRIGHT + "    ────────────────────────────────")
-        print(Fore.MAGENTA + Style.BRIGHT + "    🧬 Powered by Cryptodai3 × YetiDAO | Buddy v2.0 🚀")
+        print(Fore.MAGENTA + Style.BRIGHT + "    🧬 Powered by Cryptodai3 × YetiDAO | Buddy v2.1 🚀")
         print(Fore.LIGHTGREEN_EX + Style.BRIGHT + "═" * 60 + "\n")
 
     def format_seconds(self, seconds):
@@ -294,7 +301,7 @@ class Brokex:
         try:
             web3 = await self.get_web3_with_check(address, use_proxy)
 
-            if contract_address == "PHRS":
+            if contract_address == self.PHRS_CONTRACT_ADDRESS:
                 balance = web3.eth.get_balance(address)
                 decimals = 18
             else:
@@ -378,7 +385,10 @@ class Brokex:
             except TransactionNotFound:
                 pass
             except Exception as e:
-                pass
+                self.log(
+                    f"{Fore.CYAN + Style.BRIGHT}   Message :{Style.RESET_ALL}"
+                    f"{Fore.YELLOW + Style.BRIGHT} [Attempt {attempt + 1}] Send TX Error: {str(e)} {Style.RESET_ALL}"
+                )
             await asyncio.sleep(2 ** attempt)
         raise Exception("Transaction Hash Not Found After Maximum Retries")
 
@@ -390,7 +400,10 @@ class Brokex:
             except TransactionNotFound:
                 pass
             except Exception as e:
-                pass
+                self.log(
+                    f"{Fore.CYAN + Style.BRIGHT}   Message :{Style.RESET_ALL}"
+                    f"{Fore.YELLOW + Style.BRIGHT} [Attempt {attempt + 1}] Wait for Receipt Error: {str(e)} {Style.RESET_ALL}"
+                )
             await asyncio.sleep(2 ** attempt)
         raise Exception("Transaction Receipt Not Found After Maximum Retries")
         
@@ -398,7 +411,7 @@ class Brokex:
         try:
             web3 = await self.get_web3_with_check(address, use_proxy)
 
-            contract_address = web3.to_checksum_address(self.CLAIM_ROUTER_ADDRESS)
+            contract_address = web3.to_checksum_address(self.FAUCET_ROUTER_ADDRESS)
             token_contract = web3.eth.contract(address=contract_address, abi=self.ERC20_CONTRACT_ABI)
             claim_data = token_contract.functions.hasClaimed(web3.to_checksum_address(address)).call()
 
@@ -414,7 +427,7 @@ class Brokex:
         try:
             web3 = await self.get_web3_with_check(address, use_proxy)
 
-            contract_address = web3.to_checksum_address(self.CLAIM_ROUTER_ADDRESS)
+            contract_address = web3.to_checksum_address(self.FAUCET_ROUTER_ADDRESS)
             token_contract = web3.eth.contract(address=contract_address, abi=self.ERC20_CONTRACT_ABI)
 
             claim_data = token_contract.functions.claim()
@@ -424,7 +437,7 @@ class Brokex:
             max_fee = max_priority_fee
 
             claim_tx = claim_data.build_transaction({
-                "from": address,
+                "from": web3.to_checksum_address(address),
                 "gas": int(estimated_gas * 1.2),
                 "maxFeePerGas": int(max_fee),
                 "maxPriorityFeePerGas": int(max_priority_fee),
@@ -513,7 +526,9 @@ class Brokex:
             
             open_amount = int(self.open_amount * (10 ** decimals))
 
-            await self.approving_token(account, address, self.TRADE_ROUTER_ADDRESS, asset_address, open_amount, use_proxy)
+            await self.approving_token(account, address, self.POOL_ROUTER_ADDRESS, asset_address, self.open_amount, use_proxy)
+
+            await self.approving_token(account, address, self.TRADE_ROUTER_ADDRESS, asset_address, self.open_amount, use_proxy)
 
             proof = await self.get_proof(address, pair, use_proxy)
             if not proof:
@@ -599,7 +614,7 @@ class Brokex:
             
             deposit_lp_amount = int(self.deposit_lp_amount * (10 ** decimals))
 
-            await self.approving_token(account, address, self.POOL_ROUTER_ADDRESS, asset_address, deposit_lp_amount, use_proxy)
+            await self.approving_token(account, address, self.POOL_ROUTER_ADDRESS, asset_address, self.deposit_lp_amount, use_proxy)
 
             token_contract = web3.eth.contract(address=web3.to_checksum_address(self.POOL_ROUTER_ADDRESS), abi=self.BROKEX_CONTRACT_ABI)
 
@@ -951,7 +966,7 @@ class Brokex:
                     continue
                 return None
         
-    async def process_check_connection(self, address: int, use_proxy: bool, rotate_proxy: bool):
+    async def process_check_connection(self, address: str, use_proxy: bool, rotate_proxy: bool):
         while True:
             proxy = self.get_next_proxy_for_account(address) if use_proxy else None
             self.log(
@@ -1218,10 +1233,11 @@ class Brokex:
             size = open_data["sizeUsd"]
 
             name = (
-                "BTC_USDT" if pair == 0 else 
-                "ETH_USDT" if pair == 1 else 
-                "SOL_USDT" if pair == 10 else 
-                "XRP_USDT" if pair == 14 else 
+                "BTC_USDT" if pair == 0 else "ETH_USDT" if pair == 1 else 
+                "SOL_USDT" if pair == 10 else "XRP_USDT" if pair == 14 else 
+                "AVAX_USDT" if pair == 5 else "DOGE_USDT" if pair == 3 else 
+                "TRX_USDT" if pair == 15 else "ADA_USDT" if pair == 16 else 
+                "SUI_USDT" if pair == 90 else "LINK_USDT" if pair == 2 else 
                 "NaN_USDT"
             )
             formatted_size = size / 10**6
